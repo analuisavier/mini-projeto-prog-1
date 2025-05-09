c6742a63ee2c4702610a6b95bb71a0878a55ef08
Lucas252291 <lucas.jlrs252291@gmail.com>
2025-05-08 21:49:52 -0300

    iniciando o projeto

---
+++ main.py
@@ -0,0 +1,21 @@
+#TODO Crie os print() e variaveis necessarias 
+dias_em_um_ano = 365
+
+dias_em_um_mes = 30
+
+dia_nascimento = 0
+mes_nascimento = 0
+ano_nascimento = 0
+#Essa variavel retorna uma string porem falata as variaveis dia, mes e ano
+data_nascimento = f"{dia_nascimento}/{mes_nascimento}/{ano_nascimento}" 
+
+
+
+dias_de_anos_vividos = (2025 - ano_nascimento) * dias_em_um_ano
+dias_de_mes_vividos = 5 * dias_em_um_mes
+total_de_dias_vividos = dias_de_anos_vividos + dias_de_mes_vividos + 14
+
+
+
+
+
